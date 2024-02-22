1a852d7cf4d7dc317c6932269d6788ab9b44599e
marto55555 <15653942+marto55555@users.noreply.github.com>
2024-02-22 14:54:51 +0200

    Press Add to Home Screen -> Scroll down to Add to Home Screen

--- dist/types/localization/locales/sr.d.ts
+++ dist/types/localization/locales/sr.d.ts
@@ -2,12 +2,12 @@ export declare const templates: {
     s1431faac1f54cde3: string;
     s1b9047d53d9f9d22: string;
     s37a9e8aec5713460: string;
-    s5c6aad7a7e4a1437: string;
     s6196153c4b0c1ea0: string;
     s8462fbd1f5c45d3b: string;
     sa5ef80b4bb9b39f8: string;
     sa7551383d1897fcc: string;
     sbee26129cda85a3e: string;
+    sdbd52a32bbbbbff8: string;
     sdfe23506a3b0cdc8: string;
     see440520357a3532: string;
     sfea652f6580ff086: string;
